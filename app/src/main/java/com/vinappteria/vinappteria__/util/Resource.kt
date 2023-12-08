fbfbab1b58b932017bfb2e17d727bd4611bcb8c1
pablomen2002 <juanpablomen2002@gmail.com>
2023-12-08 11:07:39 -0600

    ach

---
+++ app/src/main/java/com/vinappteria/vinappteria__/util/Resource.kt
@@ -0,0 +1,13 @@
+package com.vinappteria.vinappteria__.util
+
+import android.provider.ContactsContract.RawContacts.Data
+import java.util.Date
+
+sealed class Resource<T>(
+    val data: T?=null,
+    val message: String?=null
+){
+    class Success<T>(data: T):Resource<T>(data)
+    class Error<T>(message: String):Resource<T>(message=message)
+    class Loading<T>: Resource<T>()
+}
--- app/src/main/java/com/vinappteria/vinappteria__/viewmodel/LoginRegisterViewModel.kt
+++ app/src/main/java/com/vinappteria/vinappteria__/viewmodel/LoginRegisterViewModel.kt
@@ -1,4 +1,35 @@
 package com.vinappteria.vinappteria__.viewmodel
 
-class LoginRegisterViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.vinappteria.vinappteria__.util.Resource
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.vinappteria.vinappteria__.data.User
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginRegisterViewModel @Inject constructor(
+    private val firebaseAuth: FirebaseAuth
+) : ViewModel() {
+
+    private val _register = MutableStateFlow<Resource<FirebaseUser>>(Resource.Loading())
+    val register:Flow<Resource<FirebaseUser>> = _register
+
+    fun createAccountWithEmailAndPassword(user: User, password:String){
+        firebaseAuth.createUserWithEmailAndPassword(user.email,password)
+            .addOnSuccessListener {
+                it.user?.let {
+                    _register.value = Resource.Success(it)
+                }
+            }.addOnFailureListener {
+                _register.value = Resource.Error(it.message.toString())
+            }
+    }
 }
